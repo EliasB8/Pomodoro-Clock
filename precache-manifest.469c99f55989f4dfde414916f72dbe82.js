5b602eb196719a3b4c280e453e7e19108cc56907
Elias Ferede <feredeelias@gmail.com>
2020-09-10 13:18:45 -0700

    Updates

---
+++ precache-manifest.469c99f55989f4dfde414916f72dbe82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38e75e9f375f792f9c91f656db38e13c",
+    "url": "/Pomodoro-Clock/index.html"
+  },
+  {
+    "revision": "1ff687043ea3f0e87a74",
+    "url": "/Pomodoro-Clock/static/css/main.b99f16f0.chunk.css"
+  },
+  {
+    "revision": "c9cf4c27b7211e12371b",
+    "url": "/Pomodoro-Clock/static/js/2.6130551b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pomodoro-Clock/static/js/2.6130551b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ff687043ea3f0e87a74",
+    "url": "/Pomodoro-Clock/static/js/main.78a2ffee.chunk.js"
+  },
+  {
+    "revision": "4ccfd468b853680e7d1a",
+    "url": "/Pomodoro-Clock/static/js/runtime-main.46c75fa3.js"
+  }
+]);
